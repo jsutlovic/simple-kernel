7c87644fb60ff4dc0fe175e7c96c0b27b5d1c201
Jero Sutlovic <jsutlovic@gmail.com>
2014-10-13 00:42:47 -0400

    Add a screen driver, some low level functions and utils. Print a message

---
+++ drivers/screen.h
@@ -0,0 +1,10 @@
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+// Attribute for our default colour scheme
+#define WHITE_ON_BLACK 0x0f
+
+// Screen device I/O ports
+#define REG_SCREEN_CTRL 0x3D4
+#define REG_SCREEN_DATA 0x3D5
+
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,20 @@
 void main() {
     // Create a pointer to a char, and point it to the first text cell of
     // video memory (i.e. the top-left of the screen)
-    char* video_memory = (char*) 0xb8000;
+    //char* video_memory = (char*) 0xb8000;
     // At the address to by video_memory, store the character 'X'
     // (i.e. display 'X' in the top-left of the scren).
-    *video_memory = 'Y';
+    //*video_memory = 'Y';
+
+    clear_screen();
+    char* msg =
+        "Hello, world!\n"
+        "This is a test\n"
+        "Of being able to print to the screen!\n"
+        "From my own 32-bit C kernel\n"
+        "Booted from 16-bit real mode\n"
+        "With an x86 assembly bootloader\n"
+        "\n"
+        "\0";
+    print(msg);
 }
